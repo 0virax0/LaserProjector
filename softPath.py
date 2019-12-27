a0d6c044d61f97c2a9311453b41a2066862cc3d5
0virax0 <filippovis@outlook.it>
2019-12-28 00:53:36 +0100

    uso array per il path, implementata suddivisione

--- softPath.py
+++ softPath.py
@@ -10,13 +10,20 @@ import numpy as np
 import math
 import networkx as nx
 
+#class Step:
+   #def __init__(self, nIndex = None, prevS = None, nextS = None, neighbourDir = None, neighbourArc = None):
+        #self.nIndex = nIndex # index of the node
+        #self.neighbourDir = neighbourDir # here I store temporarily my position relative to the node I am analyzing
+        #self.neighbourArc = neighbourArc # temporary arc formed with analyzing node
+        #self.prevS = prevS    
+        #self.nextS = nextS
+
+
 class Step:
-   def __init__(self, nIndex = None, prevS = None, nextS = None, neighbourDir = None, neighbourArc = None):
+   def __init__(self, nIndex = None, neighbourDir = None, neighbourArc = None):
         self.nIndex = nIndex # index of the node
         self.neighbourDir = neighbourDir # here I store temporarily my position relative to the node I am analyzing
         self.neighbourArc = neighbourArc # temporary arc formed with analyzing node
-        self.prevS = prevS    
-        self.nextS = nextS
 
 class Path:
     def __init__(self, step = None, length = 0):
@@ -51,70 +58,93 @@ class Path:
 
 def softPath(graph, nodePositions, initPath):    #graph nodes containing vertex indexes and list of node positions ordered by index in the graph
     # construct Path double linked list from initPath
-    if len(initPath) is 0:
+    if len(initPath) <= 1:
         return
     
-    path = Path()
-    preparedNode = None 
-    for index in initPath:
-        newNode = Step(index, preparedNode, None, None, None)
-        if preparedNode is not None:
-            preparedNode.nextS = newNode
-        newNode.prevS = preparedNode
-        preparedNode = newNode
-        #first node
-        if path.head is None:
-            path.head = newNode
-    # closing the list in a loop
-    preparedNode.nextS = path.head
-    path.head.prevS = preparedNode
-    path.length = len(initPath) # set list total length
+    #path = Path()
+    #preparedNode = None 
+    #for index in initPath:
+        #newNode = Step(index, preparedNode, None, None, None)
+        #if preparedNode is not None:
+            #preparedNode.nextS = newNode
+        #newNode.prevS = preparedNode
+        #preparedNode = newNode
+        ##first node
+        #if path.head is None:
+            #path.head = newNode
+    ## closing the list in a loop
+    #preparedNode.nextS = path.head
+    #path.head.prevS = preparedNode
+    #path.length = len(initPath) # set list total length
 
-    # from path i organize nIndexes aggregating same nIndex so that i recognize
-    # every time I pass on that node
-    # I point directly to the step in the path
-    aggrNindex = {} # nIndex: (index of the node in graph)
-    currStep = path.head
-    for _ in range(path.length):
-        if currStep.nIndex not in aggrNindex:
-            aggrNindex[currStep.nIndex] = [currStep] 
-        else:
-            aggrNindex[currStep.nIndex].append(currStep)
-
-        currStep = currStep.nextS
+    path = []
+    for nIndex in initPath:
+        path.append(Step(nIndex, None, None))
+    pathLen = len(path)
 
     # for each node get its data and that of all neighbours
     for nIndex, _ in graph.adj.items():
+        # aggregate nodes positions in the path (may have scrumbled from previous step and I need them in order)
+        aggrNindex = aggregateNodes(path) # nIndex(index of the node in graph) : i (index of step in the path)
+
         # get every neighbour's step so I can update data directly in the list
-        neighbours = getNeighbours(aggrNindex, nIndex)
+        neighbours = getNeighbours(aggrNindex, path, nIndex, pathLen)
 
-        setNeighboursInfo(nIndex, neighbours, nodePositions)
+        setNeighboursInfo(nIndex, path, neighbours, nodePositions)
 
-        # I create a map to quickly find the other end of the loop
-        loopsMap = getLoops(neighbours)
+        # I create a map to quickly find the other end of the loop (needs neighbours to be ordered sequentially with respect to the path)
+        loopsMap = getLoops(path, neighbours)
 
+        # I order neighbours based on their arc relative to this node
+        sortedNeighbours = orderNeighbours(path, neighbours)
 
+        divisionIndex = divideNeighbours(path, sortedNeighbours)
+        print(divisionIndex)
         #!!! Ad ogni passo devo risettare la aggrNindex map in modo che le occorrenze dello stesso nodo siano ordinate secondo la lista
 
-def getNeighbours(aggrNindexTable, nIndex):
+def aggregateNodes(path):
+    # from path i organize nIndexes aggregating same nIndex so that i recognize
+    # every time I pass on that node
+    aggrNindex = {} # nIndex: (index of the node in graph)
+    #currStep = path.head
+    #for _ in range(path.length):
+        #if currStep.nIndex not in aggrNindex:
+            #aggrNindex[currStep.nIndex] = [currStep] 
+        #else:
+            #aggrNindex[currStep.nIndex].append(currStep)
+
+        #currStep = currStep.nextS
+    for i, step in enumerate(path):
+        if step.nIndex not in aggrNindex:
+            aggrNindex[step.nIndex] = [i] 
+        else:
+            aggrNindex[step.nIndex].append(i)
+    
+    return aggrNindex
+
+def getNeighbours(aggrNindexTable, path, nIndex, pathLen):
     neighbours = [] # pointers to their steps
+    #for reencouter in aggrNindexTable[nIndex]:
+        #if reencouter.prevS is not None:
+            #neighbours.append(reencouter.prevS)
+        #if reencouter.nextS is not None:
+            #neighbours.append(reencouter.nextS)
     for reencouter in aggrNindexTable[nIndex]:
-        if reencouter.prevS is not None:
-            neighbours.append(reencouter.prevS)
-        if reencouter.nextS is not None:
-            neighbours.append(reencouter.nextS)
+        neighbours.append((reencouter-1) % pathLen)
+        neighbours.append((reencouter+1) % pathLen)
+    
     return neighbours
 
-def setNeighboursInfo(nIndex, neighbours, nodePositions):
+def setNeighboursInfo(nIndex, path, neighbours, nodePositions):
     for neighbour in neighbours:
-        dir = np.subtract(nodePositions[neighbour.nIndex], nodePositions[nIndex])  #direction from this node to this neighbour
+        dir = np.subtract(nodePositions[path[neighbour].nIndex], nodePositions[nIndex])  #direction from this node to this neighbour
         dir = normalize(dir)
-        neighbour.neighbourDir = dir
+        path[neighbour].neighbourDir = dir
         # calculate absolute dot of neighbour direction
         absDot = arcBetween((1,0), dir)
-        neighbour.neighbourArc = absDot
+        path[neighbour].neighbourArc = absDot
 
-def getLoops(neighbours):
+def getLoops(path, neighbours):
     # I suppose the neighbours are in order with respect to the current path
     loopsMap = {}
 
@@ -123,9 +153,25 @@ def getLoops(neighbours):
         ends = (neighbours[(i*2+1) % len(neighbours)], neighbours[(i*2+2) % len(neighbours)])
         loopsMap[ends[0]] = ends[1]
         loopsMap[ends[1]] = ends[0]
-        print((ends[0].nIndex, ends[1].nIndex))
 
     return loopsMap
+
+def orderNeighbours(path, neighbours):
+    return sorted(neighbours, key=lambda stepIndex: path[stepIndex].neighbourArc)
+
+def divideNeighbours(path, sortedNeighbours):
+    length = len(sortedNeighbours)
+    if length <= 2:
+        return 0
+
+    startIndex = 0
+    def getDir(index):
+        return path[sortedNeighbours[index]].neighbourDir
+
+    while arcBetween(getDir(startIndex), getDir((startIndex + math.ceil((length/2) - 1)) % length)) >= 0.5 or arcBetween(getDir(startIndex), getDir((startIndex + math.floor((length/2))) % length)) < 0.5:
+        startIndex = (startIndex + 1) % length
+        print("arc before ",arcBetween(getDir(startIndex), getDir((startIndex + math.ceil((length/2) - 1)) % length)), " arc after ",arcBetween(getDir(startIndex), getDir((startIndex + math.floor((length/2))) % length)))
+    return startIndex
 
 def arcBetween(v1, v2): # v1, v2 should be normalized
     #get a pseudo dot product for comparations that goes from 0(0°) to 1(360°)
