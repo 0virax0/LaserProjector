308593637164a7795bef0e8904913035f2b64434
unknown <filippovis@outlook.it>
2020-02-12 17:47:24 +0100

    continued modifying

--- graphLoader.py
+++ graphLoader.py
@@ -5,8 +5,16 @@ import networkx as nx
 class graphImporter:    
     """imports from a graphML file rapresenting the adjacency matrix"""
     weightedGraph = {}
+    vertices = []
 
     def __init__(self, filename):
-        self.weightedGraph = nx.read_graphml(filename, node_type= int, edge_key_type=<class 'int'>)
+        self.weightedGraph = nx.read_graphml(filename, node_type= int, edge_key_type=int)
+        print(self.weightedGraph)
 
-g = graphImporter("graph.csv")
+        # get positions of nodes
+        for n in self.weightedGraph.nodes():
+            self.vertices.append([self.weightedGraph.node[n]['x'], self.weightedGraph.node[n]['y']])
+        
+        # normalize positions
+
+g = graphImporter("graph.graphml")
